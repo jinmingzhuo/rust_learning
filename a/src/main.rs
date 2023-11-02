3a530f903283a9c174b5ca37acb55222a8c8fc71
jmz <jmz@DESKTOP-jmz>
2023-11-03 05:47:52 +0800

    test2

--- a/src/main.rs
+++ a/src/main.rs
@@ -1,3 +1,18 @@
+enum MyEnum {
+    A = 1,
+    B,
+    C,
+}
+
 fn main() {
-    println!("Hello, world!");
+    // 将枚举转换成整数，顺利通过
+    let x = MyEnum::C as i32;
+    println!("{}", x);
+    // 将整数转换为枚举，失败
+    // match x {
+    //     MyEnum::A => {}
+    //     MyEnum::B => {}
+    //     MyEnum::C => {}
+    //     _ => {}
+    // }
 }
