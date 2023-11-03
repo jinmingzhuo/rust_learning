aa040aaaf8efcb9f62bfd69cc782e6a45b58cd32
jmz <jmz@DESKTOP-jmz>
2023-11-04 07:04:16 +0800

    test4

--- a/src/main.rs
+++ a/src/main.rs
@@ -1,3 +1,42 @@
+trait Draw {
+    fn draw(&mut self);
+}
+struct Button {
+    id: u32,
+}
+impl Draw for Button {
+    fn draw(&mut self) {
+        println!("this is the id of botton: {}", self.id);
+    }
+}
+struct Select {
+    id: u32,
+}
+impl Draw for Select {
+    fn draw(&mut self) {
+        println!("select id {}", self.id);
+    }
+}
 fn main() {
-    println!("Hello, world!");
+    let b = Button { id: 11 };
+    let s = Select { id: 12 };
+    let bb = Box::new(b);
+    let bs = Box::new(s);
+    let v: Vec<Box<dyn Draw>> = vec![bb, bs];
+    for mut ele in v {
+        ele.draw();
+    }
+    println!("{}", get_static_str());
+}
+fn get_static_str() -> &'static str {
+    let mut s = String::new();
+    s.push_str("aaa");
+
+    // let a = Box::leak(Box::new(s));
+    let a = Box::leak(s.into_boxed_str());
+    a
+}
+/* Make it work with const generics */
+fn my_function<const N: usize>() -> [u32; N] {
+    [123; N]
 }
